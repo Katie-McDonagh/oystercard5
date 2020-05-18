c9cdfa05f028b8335ee02fbce43a1a21eb66dbe0
Student <student@makerss-air-2.mynet>
2020-05-18 15:40:22 +0100

    added argument to top_up method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,8 +6,8 @@ class Oystercard
     @balance = 0
   end
 
-  def top_up
+  def top_up(money)
 
-  end 
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,6 +5,6 @@ describe Oystercard do
     expect(subject.instance_variable_get :@balance).to eq(0)
   end
   it 'respond to top_up method' do
-    expect(subject).to respond_to(:top_up)
+    expect(subject).to respond_to(:top_up).with(1).argument
   end
 end
