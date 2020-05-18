ccfe7a4d9337bd67aa4c412d70bdda0d4e7b660f
Student <student@makerss-air-2.mynet>
2020-05-18 15:36:48 +0100

    added top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,7 @@ describe Oystercard do
   it 'balance has default balance of 0' do
     expect(subject.instance_variable_get :@balance).to eq(0)
   end
+  it 'respond to top_up method' do
+    expect(subject).to respond_to(:top_up)
+  end
 end
